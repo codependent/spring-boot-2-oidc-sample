948063b6b6deb02b1110eaabbc8a7d244461ea2c
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-12-26 15:24:54 +0100

    minor change

--- resource-server-2/src/main/kotlin/com/codependent/oidc/resourceserver2/configuration/OAuth2Config.kt
+++ resource-server-2/src/main/kotlin/com/codependent/oidc/resourceserver2/configuration/OAuth2Config.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.converter.Converter
 import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.GrantedAuthority
@@ -76,9 +77,7 @@ class OAuth2Config {
         return http.authorizeExchange { exchanges ->
             exchanges
                     .anyExchange().authenticated()
-
-        }.oauth2Login {
-        }
+        }.oauth2Login(withDefaults())
                 .logout { logout ->
                     logout.logoutUrl("/resource-server-2/logout")
                     logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
