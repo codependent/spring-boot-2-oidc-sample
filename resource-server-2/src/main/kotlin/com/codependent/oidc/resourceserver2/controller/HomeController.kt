f1e6d56caebc0df1cc4a08b7b0f0de5683a88d5e
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-12-26 13:23:09 +0100

    ReactiveSecurityContextHolder

--- resource-server-2/src/main/kotlin/com/codependent/oidc/resourceserver2/controller/HomeController.kt
+++ resource-server-2/src/main/kotlin/com/codependent/oidc/resourceserver2/controller/HomeController.kt
@@ -1,7 +1,7 @@
 package com.codependent.oidc.resourceserver2.controller
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
 import org.springframework.security.oauth2.core.user.OAuth2User
@@ -20,15 +20,17 @@ class HomeController(private val webClient: WebClient) {
     @GetMapping
     fun home(@RegisteredOAuth2AuthorizedClient authorizedClient: OAuth2AuthorizedClient,
              @AuthenticationPrincipal oauth2User: OAuth2User): Mono<String> {
-        val authentication = SecurityContextHolder.getContext().authentication
-        println(authentication)
-
-        return webClient.get().uri("http://localhost:8181/resource-server-1/rest/hello").retrieve()
-                .bodyToMono(Pair::class.java)
-                .onErrorResume { Mono.empty() }
-                .doOnNext { println(it) }
-                .doOnError { it.printStackTrace() }
-                .map { "home" }
+        return ReactiveSecurityContextHolder.getContext()
+                .doOnNext {
+                    println(it.authentication)
+                }.flatMap {
+                    webClient.get().uri("http://localhost:8181/resource-server-1/rest/hello").retrieve()
+                            .bodyToMono(Pair::class.java)
+                            .onErrorResume { Mono.empty() }
+                            .doOnNext { println(it) }
+                            .doOnError { it.printStackTrace() }
+                            .map { "home" }
+                }
     }
 
 }
