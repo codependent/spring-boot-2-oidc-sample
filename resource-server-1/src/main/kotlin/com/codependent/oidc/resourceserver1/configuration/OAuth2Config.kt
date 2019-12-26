066225946d6c180e3dd9e1b739c7c946edba8a45
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-12-26 15:17:01 +0100

    granted authorities custom mapping

--- resource-server-1/src/main/kotlin/com/codependent/oidc/resourceserver1/configuration/OAuth2Config.kt
+++ resource-server-1/src/main/kotlin/com/codependent/oidc/resourceserver1/configuration/OAuth2Config.kt
@@ -4,9 +4,14 @@ import com.codependent.oidc.resourceserver1.client.ServletBearerExchangeFilterFu
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
@@ -15,10 +20,16 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepo
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.JwtDecoders
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 import java.net.URI
 
 
@@ -27,6 +38,8 @@ import java.net.URI
  */
 @Configuration
 class OAuth2Config : WebSecurityConfigurerAdapter() {
+
+    private val authorities = mapOf("codependent" to setOf("viewer-resource-server-1", "editor-resource-server-1"))
 
     @Autowired
     lateinit var oAuth2AuthorizedClientRepository: OAuth2AuthorizedClientRepository
@@ -43,6 +56,60 @@ class OAuth2Config : WebSecurityConfigurerAdapter() {
     @Bean
     fun clientRegistrationRepository(): ClientRegistrationRepository {
         return InMemoryClientRegistrationRepository(keycloakClientRegistration())
+    }
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        return JwtDecoders.fromIssuerLocation("http://localhost:8080/auth/realms/insight")
+    }
+
+    @Bean
+    fun userAuthoritiesMapper(): GrantedAuthoritiesMapper {
+        return GrantedAuthoritiesMapper { authorities ->
+            val mappedAuthorities = mutableSetOf<GrantedAuthority>()
+
+            authorities.forEach { authority ->
+                if (authority is OidcUserAuthority) {
+                    val oidcUserAuthority = authority
+
+                    val idToken = oidcUserAuthority.idToken
+                    val userInfo = oidcUserAuthority.userInfo
+
+                    mappedAuthorities.add(SimpleGrantedAuthority(authority.authority))
+
+                    // Map the claims found in idToken and/or userInfo
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+                    this.authorities[userInfo.preferredUsername]?.forEach {
+                        mappedAuthorities.add(SimpleGrantedAuthority("ROLE_$it"))
+                    }
+
+                } else if (authority is OAuth2UserAuthority) {
+                    val oauth2UserAuthority = authority
+
+                    val userAttributes = oauth2UserAuthority.attributes
+
+                    // Map the attributes found in userAttributes
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+
+                }
+            }
+            mappedAuthorities
+        }
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests { authorizeRequests ->
+            authorizeRequests
+                    .anyRequest().authenticated()
+        }.oauth2Login(withDefaults())
+                .logout { logout ->
+                    logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
+                }
+                .oauth2ResourceServer()
+                .jwt()
+                .decoder(jwtDecoder())
+                .jwtAuthenticationConverter(grantedAuthoritiesExtractor())
+
     }
 
     private fun keycloakClientRegistration(): ClientRegistration {
@@ -64,27 +131,24 @@ class OAuth2Config : WebSecurityConfigurerAdapter() {
                 .build()
     }
 
-    @Bean
-    fun jwtDecoder(): JwtDecoder {
-        return JwtDecoders.fromIssuerLocation("http://localhost:8080/auth/realms/insight")
-    }
-
-    override fun configure(http: HttpSecurity) {
-        http.authorizeRequests { authorizeRequests ->
-            authorizeRequests
-                    .anyRequest().authenticated()
-        }.oauth2Login(withDefaults())
-                .logout { logout ->
-                    logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
-                }
-                .oauth2ResourceServer()
-                .jwt()
-                .decoder(jwtDecoder())
-    }
-
     private fun oidcLogoutSuccessHandler(): LogoutSuccessHandler? {
         val oidcLogoutSuccessHandler = OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository())
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8181/resource-server-1"))
         return oidcLogoutSuccessHandler
     }
+
+    private fun grantedAuthoritiesExtractor(): Converter<Jwt, AbstractAuthenticationToken> {
+        return GrantedAuthoritiesExtractor()
+    }
+
+    inner class GrantedAuthoritiesExtractor : JwtAuthenticationConverter() {
+        override fun extractAuthorities(jwt: Jwt): MutableCollection<GrantedAuthority> {
+            val extractedAuthorities = super.extractAuthorities(jwt)
+            authorities[jwt.getClaimAsString("preferred_username")]?.forEach {
+                extractedAuthorities.add(SimpleGrantedAuthority("ROLE_$it"))
+            }
+            return extractedAuthorities
+        }
+    }
+
 }
--- resource-server-2/src/main/kotlin/com/codependent/oidc/resourceserver2/configuration/OAuth2Config.kt
+++ resource-server-2/src/main/kotlin/com/codependent/oidc/resourceserver2/configuration/OAuth2Config.kt
@@ -5,23 +5,40 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.web.ServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcReactiveOAuth2UserService
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.server.WebFilter
+import reactor.core.publisher.Mono
 import java.net.URI
 
 
@@ -31,6 +48,8 @@ import java.net.URI
 @Configuration
 @EnableWebFluxSecurity
 class OAuth2Config {
+
+    private val authorities = mapOf("codependent" to setOf("viewer-resource-server-2", "editor-resource-server-2"))
 
     @Autowired
     lateinit var serverProperties: ServerProperties
@@ -57,32 +76,14 @@ class OAuth2Config {
         return ReactiveJwtDecoders.fromIssuerLocation("http://localhost:8080/auth/realms/insight")
     }
 
-    private fun keycloakClientRegistration(): ClientRegistration {
-        return ClientRegistration
-                .withRegistrationId("resource-server-2")
-                .clientId("resource-server-2")
-                .clientSecret("e8775c0b-2a5c-4a4c-9aee-db0678c9d59d")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
-                .scope("openid", "profile", "email", "address", "phone")
-                .authorizationUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/auth")
-                .tokenUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/token")
-                .userInfoUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/userinfo")
-                .userNameAttributeName(IdTokenClaimNames.SUB)
-                .jwkSetUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/certs")
-                .clientName("Keycloak")
-                .providerConfigurationMetadata(mapOf("end_session_endpoint" to "http://localhost:8080/auth/realms/insight/protocol/openid-connect/logout"))
-                .build()
-    }
-
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http.authorizeExchange { exchanges ->
             exchanges
                     .anyExchange().authenticated()
 
-        }.oauth2Login(withDefaults())
+        }.oauth2Login {
+        }
                 .logout { logout ->
                     logout.logoutUrl("/resource-server-2/logout")
                     logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
@@ -90,6 +91,7 @@ class OAuth2Config {
                 .oauth2ResourceServer { resourceServer ->
                     resourceServer.jwt()
                             .jwtDecoder(jwtDecoder())
+                            .jwtAuthenticationConverter(grantedAuthoritiesExtractor())
                 }
                 .build()
     }
@@ -110,9 +112,72 @@ class OAuth2Config {
         }
     }
 
+    @Bean
+    fun oidcUserService(): ReactiveOAuth2UserService<OidcUserRequest, OidcUser>  {
+        val delegate = OidcReactiveOAuth2UserService()
+
+        return ReactiveOAuth2UserService { userRequest ->
+            val oidcUser = delegate.loadUser(userRequest)
+                    .map {oidcUser ->
+                        val accessToken = userRequest.accessToken
+                        val mappedAuthorities = mutableSetOf<GrantedAuthority>()
+
+                        // 1) Fetch the authority information from the protected resource using accessToken
+                        // 2) Map the authority information to one or more GrantedAuthority's and add it to mappedAuthorities
+
+                        oidcUser.authorities.forEach {
+                            mappedAuthorities.add(SimpleGrantedAuthority(it.authority))
+                        }
+
+                        this.authorities[oidcUser.preferredUsername]?.forEach {
+                            mappedAuthorities.add(SimpleGrantedAuthority("ROLE_$it"))
+                        }
+
+                        // 3) Create a copy of oidcUser but use the mappedAuthorities instead
+                        DefaultOidcUser(mappedAuthorities, oidcUser.idToken, oidcUser.userInfo) as OidcUser
+                    }
+            oidcUser
+        }
+    }
+
+    private fun keycloakClientRegistration(): ClientRegistration {
+        return ClientRegistration
+                .withRegistrationId("resource-server-2")
+                .clientId("resource-server-2")
+                .clientSecret("e8775c0b-2a5c-4a4c-9aee-db0678c9d59d")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("openid", "profile", "email", "address", "phone")
+                .authorizationUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/auth")
+                .tokenUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/token")
+                .userInfoUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("http://localhost:8080/auth/realms/insight/protocol/openid-connect/certs")
+                .clientName("Keycloak")
+                .providerConfigurationMetadata(mapOf("end_session_endpoint" to "http://localhost:8080/auth/realms/insight/protocol/openid-connect/logout"))
+                .build()
+    }
+
     private fun oidcLogoutSuccessHandler(): ServerLogoutSuccessHandler? {
         val oidcLogoutSuccessHandler = OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository())
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8282/resource-server-2"))
         return oidcLogoutSuccessHandler
     }
+
+    private fun grantedAuthoritiesExtractor(): Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+        val extractor = GrantedAuthoritiesExtractor()
+        return ReactiveJwtAuthenticationConverterAdapter(extractor)
+    }
+
+    inner class GrantedAuthoritiesExtractor : JwtAuthenticationConverter() {
+        override fun extractAuthorities(jwt: Jwt): MutableCollection<GrantedAuthority> {
+            val extractedAuthorities = super.extractAuthorities(jwt)
+            authorities[jwt.getClaimAsString("preferred_username")]?.forEach {
+                extractedAuthorities.add(SimpleGrantedAuthority("ROLE_$it"))
+            }
+            return extractedAuthorities
+        }
+    }
+
 }
